9a4d70a1aaf3dbfc7be45c1f4e0c8a25ac4eceec
Sathsara <sathsara@efuturesworld.com>
2023-01-16 20:12:32 +0530

    initial commit

---
+++ src/app/_fake/users.table.ts
@@ -0,0 +1,67 @@
+export class UsersTable {
+  public static users: any = [
+    {
+      id: 1,
+      username: 'admin',
+      password: 'demo',
+      email: 'admin@sinque.co',
+      authToken: 'auth-token-8f3ae836da744329a6f93bf20594b5cc',
+      refreshToken: 'auth-token-f8c137a2c98743f48b643e71161d90aa',
+      roles: [1], // Administrator
+      pic: './assets/media/avatars/renato.jpg',
+      fullname: 'Renato Romani',
+      firstname: 'Renato',
+      lastname: 'Romani',
+      occupation: 'CEO',
+      companyName: 'Sinque',
+      phone: '456669067890',
+      language: 'en',
+      timeZone: 'International Date Line West',
+      website: 'https://keenthemes.com',
+      emailSettings: {
+        emailNotification: true,
+        sendCopyToPersonalEmail: false,
+        activityRelatesEmail: {
+          youHaveNewNotifications: false,
+          youAreSentADirectMessage: false,
+          someoneAddsYouAsAsAConnection: true,
+          uponNewOrder: false,
+          newMembershipApproval: false,
+          memberRegistration: true,
+        },
+        updatesFromKeenthemes: {
+          newsAboutKeenthemesProductsAndFeatureUpdates: false,
+          tipsOnGettingMoreOutOfKeen: false,
+          thingsYouMissedSindeYouLastLoggedIntoKeen: true,
+          newsAboutMetronicOnPartnerProductsAndOtherServices: true,
+          tipsOnMetronicBusinessProducts: true,
+        },
+      },
+      communication: {
+        email: true,
+        sms: true,
+        phone: false,
+      },
+      address: {
+        addressLine: 'L-12-20 Vertex, Cybersquare',
+        city: 'San Francisco',
+        state: 'California',
+        postCode: '45000',
+      },
+      socialNetworks: {
+        linkedIn: 'https://linkedin.com/admin',
+        facebook: 'https://facebook.com/admin',
+        twitter: 'https://twitter.com/admin',
+        instagram: 'https://instagram.com/admin',
+      },
+    },
+  ];
+
+  public static tokens: any = [
+    {
+      id: 1,
+      authToken: 'auth-token-' + Math.random(),
+      refreshToken: 'auth-token-' + Math.random(),
+    },
+  ];
+}
---
+++ src/app/resources/layout/layout.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InlineSVGModule } from 'ng-inline-svg-2';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  NgbDropdownModule,
+  NgbProgressbarModule,
+  NgbTooltipModule,
+} from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationModule } from '../../modules/i18n';
+import { LayoutComponent } from './layout.component';
+import { DashboatdRouting } from '../../dashboard/dashboatd-routing';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ScriptsInitComponent } from './components/scripts-init/scripts-init.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { PageTitleComponent } from './components/header/page-title/page-title.component';
+import {
+  DropdownMenusModule, ExtrasModule,
+  ModalsModule,
+} from '../partials';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarLogoComponent } from './components/sidebar/sidebar-logo/sidebar-logo.component';
+import { SidebarMenuComponent } from './components/sidebar/sidebar-menu/sidebar-menu.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: DashboatdRouting,
+  },
+];
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    ScriptsInitComponent,
+    TopbarComponent,
+    PageTitleComponent,
+    SidebarComponent,
+    SidebarLogoComponent,
+    SidebarMenuComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    TranslationModule,
+    InlineSVGModule,
+    NgbDropdownModule,
+    NgbProgressbarModule,
+    ModalsModule,
+    DropdownMenusModule,
+    NgbTooltipModule,
+    TranslateModule,
+    ExtrasModule,
+  ],
+  exports: [RouterModule],
+})
+export class LayoutModule {}
