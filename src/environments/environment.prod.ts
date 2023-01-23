9b64bf7ab7004fd5a43c18bb4db4372359b0fafc
Sathsara <sathsara@efuturesworld.com>
2023-01-23 22:29:39 +0530

    add  changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  appVersion: 'v8.1.6',
-  USERDATA_KEY: 'authf649fc9a5f55',
-  isMockEnabled: true,
-  apiUrl: 'api',
-  appThemeName: 'Sinque',
+  production: false,
+  API_URL: '$API_URL',
+  APP_URL: '$APP_URL',
 };
 
 /*
